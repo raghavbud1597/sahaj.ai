553b612d48dd878f82fa1e4828c89021603d8e9d
raghav-budhiraja <raghav.budhiraja@freshworks.com>
2021-02-03 22:28:05 +0530

    adding mail detail page

--- src/EmailDetail/index.js
+++ src/EmailDetail/index.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { 
+    Button,
+    Row 
+ } from 'react-bootstrap';
+import {getEmailList} from './../actions';
+import {useSelector, useDispatch } from 'react-redux';
+import styled from 'styled-components';
 
-export default function EmailDetail() {
+const EmailSection = styled.div`
+    background-color: #FFF;
+    height: calc(100vh - 100px);
+`
+
+export default function EmailDetail({match}) {
+    const emails = useSelector(state => state.emails);
+    const dispatch = useDispatch();
+    const email = emails?.emailList?.filter((e) => {
+        return e.id == match.params.id;
+    });
+
+    useEffect(() => {
+        dispatch(getEmailList());
+    }, []);
+
     return (
-        <div >
-        </div>
+        <EmailSection className="p-5">
+            <h1 className="text-secondary py-3 border-bottom">{email ? email[0]?.title : ''}</h1>
+            <h3 className="py-3 border-bottom">{email ? email[0]?.name : ''} <small className="text-muted "> &lt; {email ? email[0]?.email : ''} 	&gt; </small></h3>
+            <p>{email ? email[0]?.description : ''}</p>
+            <Row className="no-gutters py-3 border-top">
+                <Button variant="secondary" className="mr-3">Reply</Button>
+                <Button variant="secondary">Forward</Button>
+            </Row>
+        </EmailSection>
     )
 }
